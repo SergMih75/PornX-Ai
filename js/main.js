670692e1e94f4d14c27698571af4d06b3239773f
SergMih75 <109480381+SergMih75@users.noreply.github.com>
2024-01-02 21:47:09 +0300

    js timer add

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-"use strict"
+let deadLine = 60
+let timer = document.querySelector('.timer')
+let install = document.querySelector('.install-btn')
+
+function countdown() {
+	let min = 0
+	let sec = 0
+
+	if (deadLine > 0) {
+		if (install.classList.contains('install-btn-disable')) {
+			install.classList.remove('install-btn-disable')
+			timer.classList.remove('timer-end')
+		}
+		deadLine = deadLine - 1
+		min = Math.floor(deadLine / 60)
+		sec = Math.floor(deadLine - min * 60)
+		timer.innerHTML = `${min} min ${sec} sec`
+	} else {
+		install.classList.add('install-btn-disable')
+		timer.classList.add('timer-end')
+	}
+}
+document.addEventListener('DOMContentLoaded', () => {
+	setInterval(countdown, 1000)
+})
